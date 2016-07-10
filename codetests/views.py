6298a17c4866019f7065398934fcb471a4eba734
paramp <paramp@cisco.com>
2016-07-10 23:39:02 +0530

    committing the codetest form

--- codetests/views.py
+++ codetests/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from coding.models import Qns
 from forms import CodeTestForm
+from models import CodeTests
 # Create your views here.
 def show(request):
-  qns = Qns.objects.all();
+  qns = CodeTests.objects.all();
   qnform = CodeTestForm() 
   return render(request, "codetests/codetests_show.html",{"form": qnform, "tests": qns})
