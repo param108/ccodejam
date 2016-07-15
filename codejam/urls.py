c15e25c6590d9e031deb802176c7828144724acf
paramp <paramp@cisco.com>
2016-07-15 20:24:40 +0530

    Bug

--- codejam/urls.py
+++ codejam/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import url, include
 from codejamlogin import views as codejamloginviews
+from codejam import settings
 import os
 os.environ['TZ']="asia/kolkata"
 
