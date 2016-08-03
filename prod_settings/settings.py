8350d57d50b5a293a40a4430d801e1473c856687
paramp <paramp@cisco.com>
2016-08-03 09:46:36 +0530

    Production should have Debug = FALSE

--- prod_settings/settings.py
+++ prod_settings/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xbl)yuhcj_lempgdo72k*&uri=ehz2)r_hsl@=w9yfe522%0w#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
